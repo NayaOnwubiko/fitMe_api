159187c2313f5759f8017df7266d96757da2ab91
NayaOnwubiko <naya.onwubiko@gmail.com>
2023-04-04 21:28:25 -0400

    set up routes

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
