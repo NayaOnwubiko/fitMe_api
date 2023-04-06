c89cdf0167be90d0e72f224726996dcb937b671e
NayaOnwubiko <naya.onwubiko@gmail.com>
2023-04-06 02:04:39 -0400

    set up user profile route

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
+const CLIENT_URL = process.env.CLIENT_URL;
+const cors = require('cors');
+
 
 app.use(express.json());
+app.use(cors({ origin: CLIENT_URL }));
 
 const userRoutes = require('./routes/user-routes');
 const scheduleRoutes = require('./routes/schedule-routes');
 
-app.use("/users", userRoutes);
+app.use("/", userRoutes);
 app.use("/schedule", scheduleRoutes);
 
 app.listen(PORT, () => {
