b0df72a5199f7f147371ca7533f585c4c35418b7
NayaOnwubiko <naya.onwubiko@gmail.com>
2023-04-05 22:19:20 -0400

    added meal data

---
+++ seed_data/meals.js
@@ -0,0 +1,170 @@
+module.exports = [
+    {
+    name: "Avocado Egg Toast",
+    type: "Breakfast",
+    vegetarian: "TRUE",
+    calories: "271",
+    ingredients: "¼ avocado, ¼ teaspoon ground pepper, ⅛ teaspoon garlic powder, 1 slice whole-wheat bread, toasted, 1 large egg, fried, 1 teaspoon Sriracha (Optional), 1 tablespoon scallion, sliced (Optional)",
+    preparation: "Combine avocado, pepper and garlic powder in a small bowl and gently mash. Top toast with the avocado mixture and fried egg. Garnish with Sriracha and scallion, if desired."
+    },
+    {
+    name: "Bran Cereal & Blueberries",
+    type: "Breakfast",
+    vegetarian: "TRUE",
+    calories: "265",
+    ingredients: "1 cup bran cereal, 1 cup skim milk, 1/4 cup blueberries",
+    preparation: "Add Cereal to bowl. Add Blueberries. Pour skim milk to desired quantity."
+    },
+    {
+    name: "Blueberry & Almond Yogurt",
+    type: "Breakfast",
+    vegetarian: "TRUE",
+    calories: "297",
+    ingredients: "1 cup nonfat plain Greek yogurt, 3/4 cup blueberries, 1 1/2 Tbsp. slivered almonds, 2 tsp. honey",
+    preparation: "In a dry frying pan, toast the flaked almonds over a low heat until they turn golden. Remove from the pan and allow to cool. Place the yogurt in a bowl and stir through the chia seeds. Top with almonds and blueberries to serve."
+    },
+    {
+    name: "Branflakes & Blueberry with Mapel Syrup",
+    type: "Breakfast",
+    vegetarian: "TRUE",
+    calories: "265",
+    ingredients: "1 Bowl Branflakes, 4 tbls Blueberries, 1 little milk, 1 tsp Mapel Syrup",
+    preparation: "Add Cereal to bowl. Add Blueberries. Pour milk & Mapel Syrup to desired quantity"
+    },
+    {
+    name: "Rasberry Oatmeal",
+    type: "Breakfast",
+    vegetarian: "TRUE",
+    calories: "333",
+    ingredients: "Lemon Zest, Salt, Raspberries, Rolled Oats, Honey, Milk.",
+    preparation: "Bring the milk to a gentle boil in a small saucepan. Place half of the raspberries into a small bowl, along with the tablespoon of honey. Stir the oatmeal, and let it continue to cook for 8 - 10 minutes, or until thickened to your liking. Stir occasionally, and turn the heat down if needed."
+    },
+    {
+    name: "Blueberry Almond Dino Cereal",
+    type: "Breakfast",
+    vegetarian: "TRUE",
+    calories: "328",
+    ingredients: "1 cup + 2 tbsp Almond Flour, 1/2 tsp Xanthan Gum, 1 tsp Baking Powder, 1 tsp Cinnamon, 1/4 cup Granulated Zero Cal Sweetener, 2 tbsp Butter, soft but not melted, 1 egg, lightly beaten, 1 tsp Vanilla, 1 cup Assorted Dried Fruit, Seeds, and Slivered Almonds",
+    preparation: "In a large mixing bowl, combine the dry ingredients (not including the sugar) and whisk together. Set aside. Scoop the batter into a large piping bag and cut the tip at about 1/2 inch. Fill each well of the mold about halfway to allow for expansion and tap the molds well to ensure the batter fills each crevice. Bake for 20 minutes or until the tops begin to brown. Once cool, pop the cereal from the molds. For additional crunch, return the cereal to the oven for 5 more minutes on a baking sheet to crisp."
+    },
+    {
+    name: "Avocado Toast, Egg & Blueberries",
+    type: "Breakfast",
+    vegetarian: "TRUE",
+    calories: "355",
+    ingredients: "2 Eggs, 1 Rippened Avocado, 2 Slices of Bread, Salt & Pepper to taste",
+    preparation: "Preheat small skillet. Add cooking spray or butter to pan. Crack eggs directly into skillet. Let cook to your desired consistency. If you like more avocado cut it in half and take out the pit. Use one side of avocado for each slice of bread. If you do not like a lot of avocado use one side of the avocado for both pieces of bread and store the rest in the refrigerator."
+    },
+    {
+    name: "Greek Salad with Edamame",
+    type: "Lunch",
+    vegetarian: "TRUE",
+    calories: "344",
+    ingredients: "¼ cup red-wine vinegar, 3 tablespoons extra-virgin olive oil, ¼ teaspoon salt, ¼ teaspoon ground pepper, 8 cups chopped romaine (about 2 romaine hearts), 16 ounces frozen shelled edamame (about 3 cups), thawed (see Tip), 1 cup halved cherry or grape tomatoes, ½ European cucumber, sliced, ½ cup crumbled feta cheese, ¼ cup slivered fresh basil, ¼ cup sliced Kalamata olives, ¼ cup slivered red onion",
+    preparation: "Whisk vinegar, oil, salt and pepper in a large bowl. Add romaine, edamame, tomatoes, cucumber, feta, basil, olives and onion. Toss to coat."
+    },
+    {
+    name: "Mason Jar Power Salad with Chickpeas & Tuna",
+    type: "Lunch",
+    vegetarian: "TRUE",
+    calories: "430",
+    ingredients: "3 cups bite-sized pieces chopped kale, 2 tablespoons honey-mustard vinaigrette (see associated recipe), 1 2.5-ounce pouch tuna in water, ½ cup rinsed canned chickpeas, 1 carrot, peeled and shredded",
+    preparation: "Toss kale and dressing in a bowl, then tranfer to a 1-quart mason jar. Top with tuna, chickpeas and carrot. Screw lid onto the jar and refrigerate for up to 2 days. To serve, empty the jar contents into a bowl and toss to combine the salad ingredients with the dressed kale."
+    },
+    {
+    name: "Green Salad with Pita Bread & Hummus",
+    type: "Lunch",
+    vegetarian: "TRUE",
+    calories: "374",
+    ingredients: "2 cups mixed salad greens, ½ cup sliced cucumber, 2 tablespoons grated carrot, 1 ½ teaspoons extra-virgin olive oil, 1 ½ teaspoons balsamic vinegar, Pinch of salt, Pinch of ground pepper, 1 6 1/2-inch whole-wheat pita bread, toasted, ¼ cup hummus",
+    preparation: "Arrange greens, cucumber and carrot on a large plate. Drizzle with oil and vinegar. Sprinkle with salt and pepper. Serve with pita and hummus."
+    },
+    {
+    name: "Kale Salad with Beets & Wild Rice",
+    type: "Lunch",
+    vegetarian: "TRUE",
+    calories: "173",
+    ingredients: "1 large bunch lacinato or curly kale, stems trimmed, chopped (8 cups), 1 medium beet, peeled, halved and very thinly sliced (2 1/2 cups), 1 cup cooked wild rice, ⅓ cup toasted sunflower seeds, 5 tablespoons Lemon-Tahini Dressing (see associated recipe)",
+    preparation: "Combine kale, beet, wild rice and sunflower seeds in a large bowl. Add dressing and toss until well coated. Serve within 2 hours."
+    },
+    {
+    name: "Slow-Cooker Creamy Lentil Soup Freezer Pack",
+    type: "Lunch",
+    vegetarian: "TRUE",
+    calories: "320",
+    ingredients: "1 cup green or brown lentils, picked over and rinsed, 1 cup chopped onions, 1 cup diced carrots, 2 tablespoons finely chopped garlic, 2 teaspoons ground coriander, 2 bay leaves, 1 teaspoon ground cumin, 1 teaspoon dried oregano, 1 teaspoon ground pepper, ¼ teaspoon cayenne pepper, 4 cups reduced-sodium chicken broth or vegetable broth, 2 cups coarsely chopped spinach, 1 (15 ounce) can diced tomatoes, ⅔ cup light coconut milk, ¼ cup chopped fresh parsley, 1 tablespoon white-wine vinegar, ½ teaspoon salt",
+    preparation: "Combine lentils, onions, carrots, garlic, coriander, bay leaves, cumin, oregano, pepper and cayenne in a sealable plastic bag. Empty the freezer bag into a 6-quart slow-cooker. Add broth. Cover and cook on High for 4 hours or Low for 8 hours. Discard bay leaves. Transfer half of the soup to a blender and puree. (Use caution when blending hot liquids.) Return the pureed soup to the slow cooker and stir in spinach, tomatoes, coconut milk, parsley, vinegar and salt; heat through."
+    },
+    {
+    name: "Spinach & Strawberry Salad",
+    type: "Lunch",
+    vegetarian: "TRUE",
+    calories: "374",
+    ingredients: "1 pound boneless, skinless chicken thighs, ½ teaspoon kosher salt, ½ teaspoon dried thyme, ½ teaspoon ground pepper, 8 cups baby spinach, 2 cups sliced strawberries, ¼ cup feta cheese (Optional), ¼ cup chopped toasted walnuts, 6 tablespoons Balsamic Vinaigrette",
+    preparation: "Place chicken on the prepared baking sheet. Sprinkle all over with salt, thyme and pepper. Roast, flipping once, until the chicken is cooked through and reaches an internal temperature of 165°F, 15 to 17 minutes. Set aside to cool, then slice into bite-size pieces. Divide spinach among 4 single-serving lidded containers (2 cups each). Top each with one-fourth of the sliced chicken, 1/2 cup sliced strawberries, 1 tablespoon feta (if using) and 1 tablespoon walnuts. Transfer 1 1/2 tablespoons vinaigrette into each of 4 small lidded containers and refrigerate for up to 5 days."
+    },
+    {
+    name: "Roasted Vegetable & Black Bean Tacos",
+    type: "Lunch",
+    vegetarian: "TRUE",
+    calories: "343",
+    ingredients: "1 cup roasted root vegetables, ½ cup cooked or canned black beans, rinsed, 2 teaspoons extra-virgin olive oil, 1 teaspoon ground cumin, 1 teaspoon chili powder, ½ teaspoon ground coriander, ¼ teaspoon kosher salt, ¼ teaspoon ground pepper, 4 corn tortillas, lightly toasted or warmed, ½ avocado, cut into 8 slices, 1 lime, cut into wedges, Chopped fresh cilantro & salsa for garnish",
+    preparation: "Combine roasted root vegetables, beans, oil, cumin, chili powder, coriander, salt and pepper in a saucepan. Cover and cook over medium-low heat until heated through, 6 to 8 minutes. Divide the mixture among the tortillas. Top with avocado. Serve with lime wedges. Garnish with cilantro and/or salsa, if desired."
+    },
+    {
+    name: "Roasted Root Veggies & Greens over Spiced Lentils",
+    type: "Dinner",
+    vegetarian: "TRUE",
+    calories: "453",
+    ingredients: "1 ½ cups water, ½ cup black beluga lentils or French green lentils (see Tip), 1 teaspoon garlic powder, ½ teaspoon ground coriander, ½ teaspoon ground cumin, ¼ teaspoon ground allspice, ¼ teaspoon kosher salt, ⅛ teaspoon sumac (optional), 2 tablespoons lemon juice, 1 teaspoon extra-virgin olive oil",
+    preparation: "To prepare lentils: Combine water, lentils, garlic powder, 1/2 teaspoon coriander, cumin, allspice, 1/4 teaspoon salt and sumac (if using) in a medium pot. Bring to a boil. Reduce heat to maintain a simmer, cover and cook until tender, 25 to 30 minutes. Heat oil in a large skillet over medium heat. Add garlic and cook until fragrant, 1 to 2 minutes. Add roasted root vegetables and cook, stirring often, until heated through, 2 to 4 minutes. Stir in kale (or beet greens) and cook until just wilted, 2 to 3 minutes. Stir in coriander, pepper and salt. Serve the vegetables over the lentils, topped with tahini (or yogurt). Garnish with parsley, if desired."
+    },
+    {
+    name: "Spaghetti Squash with Roasted Tomatoes, Beans & Almond Pesto",
+    type: "Dinner",
+    vegetarian: "TRUE",
+    calories: "400",
+    ingredients: "2 cups fresh basil leaves, 1 cup fresh parsley leaves, ½ cup grated Parmesan cheese, ⅓ cup whole raw almonds, 1 clove garlic, 1 ½ tablespoons red-wine vinegar, ¼ teaspoon kosher salt, ¼ teaspoon ground pepper, ¼ cup extra-virgin olive oil, ¼ cup water",
+    preparation: "To prepare pesto: Pulse basil, parsley, Parmesan, almonds, garlic, vinegar and 1/4 teaspoon each salt and pepper in a food processor until coarsely chopped, scraping down the sides. With the motor running, add 1/4 cup oil; process until well combined. To prepare squash & vegetables: Preheat oven to 400 degrees F. Line a rimmed baking sheet with foil. Scrape the squash flesh into the bowl and divide among 4 plates. Top each portion with some of the tomato-bean mixture and about 3 tablespoons pesto sauce."
+    },
+    {
+    name: "Squash & Red Lentil Curry",
+    type: "Dinner",
+    vegetarian: "TRUE",
+    calories: "326",
+    ingredients: "2 cups fresh basil leaves, 1 cup fresh parsley leaves, ½ cup grated Parmesan cheese, ⅓ cup whole raw almonds, 1 clove garlic, 1 ½ tablespoons red-wine vinegar, ¼ teaspoon kosher salt, ¼ teaspoon ground pepper, ¼ cup extra-virgin olive oil, ¼ cup water",
+    preparation: "Heat oil in a large pot over medium-high heat. Add onion, garlic, ginger and curry powder (or garam masala); cook, stirring often, until the onion is starting to soften, 2 to 3 minutes. Add squash, lentils, tomato and salt; cook, stirring, for 1 minute. Add water. Cover and bring to a boil over high heat. Reduce heat to maintain a lively simmer; cook, covered, stirring occasionally, until the squash is tender and the lentils are mostly broken down, about 20 minutes. Stir in coconut milk and simmer until heated through, about 1 minute. Serve with lime wedges and cilantro, if desired."
+    },
+    {
+    name: "Tofu & Vegetable Scramble",
+    type: "Dinner",
+    vegetarian: "TRUE",
+    calories: "420",
+    ingredients: "1 ½ teaspoons extra-virgin olive oil, 5 ounces extra-firm tofu, drained and cubed, 1 cup chopped vegetables, such as zucchini, mushrooms and onions, ½ teaspoon spice of choice, such as chili powder or ground cumin, Pinch of ground pepper, ⅓ cup canned chickpeas, rinsed, ¼ cup pico de gallo or salsa, ¼ cup shredded Cheddar cheese, preferably sharp (1 oz.), 1 dash Hot sauce and chopped cilantro to taste",
+    preparation: "Heat oil in a large nonstick skillet over medium-high heat. Add tofu, vegetables, spice and pepper; cook, stirring often, until the vegetables are softened, 5 to 7 minutes. Add chickpeas and pico de gallo (or salsa) and heat through, 1 to 2 minutes. Remove from heat, gather the scramble into one section of the pan, top with Cheddar cheese and let melt off the heat. Serve with hot sauce and cilantro, if desired."
+    },
+    {
+    name: "Kale Salad with Beets & Wild Rice",
+    type: "Dinner",
+    vegetarian: "TRUE",
+    calories: "173",
+    ingredients: "1 large bunch lacinato or curly kale, stems trimmed, chopped (8 cups), 1 medium beet, peeled, halved and very thinly sliced (2 1/2 cups), 1 cup cooked wild rice, ⅓ cup toasted sunflower seeds, 5 tablespoons Lemon-Tahini Dressing (see associated recipe)",
+    preparation: "Combine kale, beet, wild rice and sunflower seeds in a large bowl. Add dressing and toss until well coated. Serve within 2 hours."
+    },
+    {
+    name: "Zoodles with Romesco",
+    type: "Dinner",
+    vegetarian: "TRUE",
+    calories: "155",
+    ingredients: "8 cups zucchini noodles (from 3 medium), ½ teaspoon salt, divided, ¼ teaspoon ground pepper, ¼ cup raw almonds, 1 medium red bell pepper, cut into chunks, 2 tablespoons cold-pressed extra-virgin olive oil, 1 small clove garlic, crushed, 1 teaspoon paprika, ½ teaspoon ground cumin, ¼ teaspoon crushed red pepper (Optional)",
+    preparation: "Toss zucchini noodles with 1/4 teaspoon salt and pepper in a large bowl. Place almonds in a mini food processor and pulse until coarsely chopped. Add red pepper, oil, garlic, paprika, cumin, crushed red pepper (if using) and the remaining 1/4 teaspoon salt; process until fairly smooth. Add 3/4 of the red pepper sauce to the zucchini and toss to coat. Serve, topped with a bit more sauce, if desired."
+    },
+    {
+    name: "Roasted Vegetable & Black Bean Tacos",
+    type: "Dinner",
+    vegetarian: "TRUE",
+    calories: "343",
+    ingredients: "1 cup roasted root vegetables, ½ cup cooked or canned black beans, rinsed, 2 teaspoons extra-virgin olive oil, 1 teaspoon ground cumin, 1 teaspoon chili powder, ½ teaspoon ground coriander, ¼ teaspoon kosher salt, ¼ teaspoon ground pepper, 4 corn tortillas, lightly toasted or warmed, ½ avocado, cut into 8 slices, 1 lime, cut into wedges, Chopped fresh cilantro & salsa for garnish",
+    preparation: "Combine roasted root vegetables, beans, oil, cumin, chili powder, coriander, salt and pepper in a saucepan. Cover and cook over medium-low heat until heated through, 6 to 8 minutes. Divide the mixture among the tortillas. Top with avocado. Serve with lime wedges. Garnish with cilantro and/or salsa, if desired."
+    },
+    ];
