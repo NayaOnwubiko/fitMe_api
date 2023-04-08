11b02d90d49d57041cb6455647b60d4d639de074
NayaOnwubiko <naya.onwubiko@gmail.com>
2023-04-08 12:39:43 -0400

    added image links

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 8080;
 const CLIENT_URL = process.env.CLIENT_URL;
 const cors = require('cors');
 
-
 app.use(express.json());
 app.use(cors({ origin: CLIENT_URL }));
+app.use("/public", express.static("./public"))
 
-const userRoutes = require('./routes/user-routes');
-const scheduleRoutes = require('./routes/schedule-routes');
+const userRoutes = require("./routes/user-routes");
+const scheduleRoutes = require("./routes/schedule-routes");
 
 app.use("/users", userRoutes);
 app.use("/schedule", scheduleRoutes);
